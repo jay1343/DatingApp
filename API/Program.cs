17d5e368f045b39bee681947a3f8657aff94c221
master dext <masterdext2@gmail.com>
2023-06-26 12:45:11 +0530

    Section 3

--- API/Program.cs
+++ API/Program.cs
@@ -9,11 +9,12 @@ builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
+app.UseCors(builder => builder.AllowAnyHeader().WithOrigins("http://localhost:4200"));
 app.MapControllers();
 
 app.Run();
